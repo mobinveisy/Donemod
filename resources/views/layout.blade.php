20329cf12c0b7d9472c7105a6de701b53fba858c
Daniel <72124248+danielrgh@users.noreply.github.com>
2021-09-16 14:33:01 +0430

    add tailwindcss and npm dependencies, design basic main page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,9 +4,190 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title> Donemod </title>
+    <style>
+      .max-w-xs2
+      {
+        max-width: 16rem;
+      }
+    </style>
 </head>
 <body>
-    Hello world
+    <div>
+        <nav class="bg-gray-800">
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex items-center justify-between h-10">
+                
+              <div class="flex items-center">
+                <div class="flex-shrink-0">
+                  <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
+                </div>
+              </div>
+
+                <div class="hidden md:block">
+                  <div class="ml-10 flex items-baseline">
+                    <a href="#" class="text-gray-300 px-3 py-2 rounded-md text-xl"> Donemod </a>
+                  </div>
+                </div>
+              
+
+              <div class="hidden md:block">
+                <div class="ml-4 flex items-center md:ml-6">
+                  <button type="button" class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <span class="sr-only">View notifications</span>
+                    <!-- Heroicon name: outline/bell -->
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                    </svg>
+                  </button>
+      
+                  <!-- Profile dropdown -->
+                  <div class="ml-3 relative">
+                    <div>
+                      <button type="button" class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                        <span class="sr-only">Open user menu</span>
+                        <img class="h-8 w-8 rounded-full" src="#" alt="">
+                      </button>
+                    </div>
+      
+                   
+                      {{-- Dropdown menu, show/hide based on menu state.
+      
+                      Entering: "transition ease-out duration-100"
+                        From: "transform opacity-0 scale-95"
+                        To: "transform opacity-100 scale-100"
+                      Leaving: "transition ease-in duration-75"
+                        From: "transform opacity-100 scale-100"
+                        To: "transform opacity-0 scale-95"
+                    
+                     <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                      <!-- Active: "bg-gray-100", Not Active: "" -->
+                      <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+      
+                      <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+      
+                      <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                    </div> --}}
+
+
+
+                  </div>
+                </div>
+              </div>
+              <div class="-mr-2 flex md:hidden">
+                <!-- Mobile menu button -->
+                <button type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                  <span class="sr-only">Open main menu</span>
+                  <!--
+                    Heroicon name: outline/menu
+      
+                    Menu open: "hidden", Menu closed: "block"
+                  -->
+                  <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                  </svg>
+                  <!--
+                    Heroicon name: outline/x
+      
+                    Menu open: "block", Menu closed: "hidden"
+                  -->
+                  <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+      
+          <!-- Mobile menu, show/hide based on menu state. -->
+          <div class="md:hidden" id="mobile-menu">
+            <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
+      
+              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+      
+              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+      
+              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+      
+              <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Reports</a>
+            </div>
+            <div class="pt-4 pb-3 border-t border-gray-700">
+              <div class="flex items-center px-5">
+                <div class="flex-shrink-0">
+                  <img class="h-10 w-10 rounded-full" scr="#" alt="user">
+                </div>
+                <div class="ml-3">
+                  <div class="text-base font-medium leading-none text-white">Tom Cook</div>
+                  <div class="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
+                </div>
+                <button type="button" class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <span class="sr-only">View notifications</span>
+                  <!-- Heroicon name: outline/bell -->
+                  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                  </svg>
+                </button>
+              </div>
+              <div class="mt-3 px-2 space-y-1">
+                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Your Profile</a>
+      
+                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Settings</a>
+      
+                <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Sign out</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      
+        {{-- <header class="bg-white shadow">
+          <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 class="text-2xl font-bold text-gray-900">
+              Hello UserName <br>
+              What do you want to do?
+            </h1>
+          </div>
+        </header> --}}
+      </div>
+
+      <div class="flex ">
+          <div class="h-auto flex-auto max-w-xs2 m-4  divide-y border-r-2">
+            Lists 
+
+            <div class="flex justify-between">
+              <p class="m-1"> <i class="fa fa-calendar" aria-hidden="true"></i> Today </p>
+              <span class="mr-1"> 9 </span>
+            </div>
+
+            <div class="flex justify-between">
+              <p class="m-1"> <i class="fa fa-star" aria-hidden="true"></i> Important </p>
+              <span class="mr-1"> 3 </span>
+            </div>
+
+            <div class="flex justify-between">
+              <p class="m-1"> <i class="fa fa-columns" aria-hidden="true"></i> Planned </p>
+              <span class="mr-1"> 2 </span>
+            </div>
+
+          </div>
+          <div class="h-auto flex-auto m-4 divide-y"> 
+            Tasks
+
+            <div class="flex flex-col m-4">
+              <p class=""><i class="fa fa-check-circle" aria-hidden="true"></i> Eating Water </p>
+              <p class=" border-black pl-5"> Today <span> Date </span></p> 
+            </div>
+            <div class="flex flex-col m-4">
+              <p class=""><i class="fa fa-check-circle" aria-hidden="true"></i> Bake a Cake </p>
+              <p class=" border-black pl-5"> Important <span> yesterday </span></p> 
+            </div>
+            
+          </div>
+          <!-- Replace with your content -->
+          <!-- /End replace -->
+      </div>
+      <script src="https://use.fontawesome.com/44d08aa6b0.js"></script>
 </body>
 </html>
